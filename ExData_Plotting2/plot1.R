3ad8f40ae77e92cd6e1cbe48b19ad72e1fe03b42
lisah2u <lisah2u@gmail.com>
2014-07-15 21:28:37 -0400

    Added plot2.r

--- ExData_Plotting2/plot1.R
+++ ExData_Plotting2/plot1.R
@@ -7,12 +7,14 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-# join on SCC code
-#df <- merge(NEI, SCC, all.x=TRUE)
-
 # plot emissions per year
+png("plot1.png")
+
 emissions <- tapply(NEI$Emissions, NEI$year, sum)
-plot(emissions, type="b", xlab="Year", ylab="PM2.5 Emissions")
+#plot(emissions, pch=16, xlab="Year", ylab="PM2.5 Emissions", main="PM2.5 Emissions in the US from 1999 to 2008", xaxt="n")
+plot(emissions, type="b", pch=16, xlab="Year", ylab="PM2.5 Emissions", main="PM2.5 Emissions in the US from 1999 to 2008", xaxt="n")
 
 # adjust x-axis to display years
-axis(1, NEI$year)
+axis(1, at=1:4, labels=unique(NEI$year))
+
+dev.off()
--- ExData_Plotting2/plot2.R
+++ ExData_Plotting2/plot2.R
@@ -1,3 +1,19 @@
 # Have total emissions from PM2.5 decreased in the Baltimore City, 
 # Maryland (fips == "24510") from 1999 to 2008? Use the base plotting 
 # system to make a plot answering this question.
+
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# plot emissions per year
+png("plot2.png")
+
+baltimore <- subset(NEI, NEI$fips == "24510")
+emissions <- tapply(baltimore$Emissions, baltimore$year, sum)
+plot(emissions, type="b", pch=16, xlab="Year", ylab="PM2.5 Emissions", main="PM2.5 Emissions in Baltimore from 1999 to 2008", xaxt="n")
+
+# adjust x-axis to display years
+axis(1, at=1:4, labels=unique(NEI$year))
+
+dev.off()
